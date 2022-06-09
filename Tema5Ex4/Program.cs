0589fbd56c8c5fb04dbbc6640fb184fc403b233b
Gergoa123 <gergo3124@gmail.com>
2022-06-09 16:44:15 +0300

    Add project files.

---
+++ Tema5Ex4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tema5Ex4
+{
+    class Program
+    {
+        //Ex 4
+        //Scrieti un program care va afisa caracterul cu numarul cel mai mare de aparitii dintr-un string citit
+        //de la tastatura, ignorand caseing-ul literelor.
+
+        static char aparitii(string a)
+        {
+            int[] count = new int[256];
+            int max = 0;
+            Char rezultat = Char.MinValue;
+
+            Array.Clear(count, 0, count.Length - 1);
+
+            foreach (Char c in a)
+            
+                if (c != ' ' && ++count[c] > max)
+                {
+                    max = count[c];
+                    rezultat = c;
+                }
+
+            return rezultat;
+           
+
+        }
+
+
+        static void Main(string[] args)
+        
+        
+        
+        {
+            Console.WriteLine("Introduceti stringul!");
+            string cuvant = Console.ReadLine();
+
+            Console.WriteLine("Caracterul care apare cel mai mult este " + aparitii(cuvant));
+
+
+        }
+    }
+}
